155cc0132bb2cee0d35b2a528b64b6d8320f7012
Sv-Namrata-Ag <sv.namrataagarwal@gmail.com>
2025-12-11 06:48:18 +0000

    basic design

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import NIETChatbot from './components/NIETChatbot'
+
+export default function App(){
+  return (
+    <div className="min-h-screen flex items-center justify-center p-6">
+      <NIETChatbot />
+    </div>
+  )
+}
