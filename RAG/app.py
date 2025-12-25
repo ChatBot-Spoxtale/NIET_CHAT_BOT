a7b9fb2782c7b7baca020102f7eaae825afe2db2
svaadityakuamr8130-sys <sv.aadityakuamr8130@gmail.com>
2025-12-25 05:34:42 +0530

    updating app.py file

--- RAG/app.py
+++ RAG/app.py
@@ -20,8 +20,7 @@ app.add_middleware(
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "https://niet-chat-bot.onrender.com/",
-    "https://niet-chat-bot-rag.onrender.com"  
-"
+    "https://niet-chat-bot-rag.onrender.com"
 ],          
     allow_credentials=True,
     allow_methods=["*"],         
