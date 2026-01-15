5666d83fa5fab59226d0b8c64a11ada1c0d6bed1
Sv-Namrata-Ag <sv.namrataagarwal@gmail.com>
2026-01-15 15:47:30 +0530

    Adding embed tag

--- Frontend/src/components/NIETChatbot.jsx
+++ Frontend/src/components/NIETChatbot.jsx
@@ -27,8 +27,14 @@ export default function NIETChatbot(embed={embed}) {
         </button>
       )}
 
-      {!embed && open && (
-        <div className="fixed inset-0 z-[90] pointer-events-none flex flex-col items-end justify-end p-2 sm:p-6 animate-in fade-in duration-300">
+      {open && (
+        <div
+  className={
+    embed
+      ? "fixed bottom-6 right-6 z-[90]"   // no fullscreen, no transparency
+      : "fixed inset-0 z-[90] pointer-events-none flex flex-col items-end justify-end p-2 sm:p-6 animate-in fade-in duration-300"
+          }
+        >
           <div className="relative pointer-events-auto w-full sm:w-[380px] md:w-[480px] lg:w-[560px] h-full sm:h-[min(700px,calc(100vh-120px))] md:h-[min(800px,calc(100vh-100px))] lg:h-[min(850px,calc(100vh-80px))] bg-white border border-slate-100 shadow-[0_30px_100px_rgba(0,0,0,0.18)] flex flex-col animate-in slide-in-from-bottom-8 duration-500 rounded-[32px] transform-gpu">
             {/* Desktop Close Button */}
             <button
